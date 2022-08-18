af268def060ca02e24c6adea385219d2e2a5a4b1
juliancolli03 <jc7236352@gmail.com>
2022-08-18 15:12:58 -0300

    tarea subida

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return(
+ <div className='container'>
+   <div className='row'>
+   <div className='col-md-4 offset-md-2'>
+
+
+ <h1> Casa Tecno</h1>
+ <p> Bienvenidos a la casa de la tecnologia de la Ciudad de Ramos Mejia.</p>
+ </div>
+ </div>
+ 
+ </div>
+ )
 }
 
 export default App;
