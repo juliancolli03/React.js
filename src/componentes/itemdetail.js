60609fffac92af7f1f4ee9b22fb4852b9acbf188
juliancolli03 <jc7236352@gmail.com>
2022-09-18 21:36:26 -0300

    desafio terminadisimo , falta estilar

--- src/componentes/itemdetail.js
+++ src/componentes/itemdetail.js
@@ -1,20 +1,32 @@
 
 
 import React from 'react';
+import ItemCount2 from "./itemcount2";
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-
 const ItemDetail = ({ item }) => {
+    const onAdd = () => {
+        setToCart(true)    }
+
+    const [goToCart, setToCart] = useState(false)
+
     return (
-        // <Link to={"/productos${item.id}"} >
         <div className="detail">
-            <img src={item.foto} />
+            <img src={item.foto} className="card-img-top rounded-top" />
             <div className="infoDetail">
                 <h2>{item.titulo}</h2>
                 <p>
                     {item.descripcion}
                 </p>
-                <h3>${item.precio}.-</h3>
-
+                <h3>${item.precio}</h3>
+                {goToCart ?
+                                <Link to="/cart"> terminar</Link>
+                :<ItemCount2 stock={item.stock}  onAdd={onAdd}  />
+                // <Link to="/cart"> terminar</Link>
+           
+                    
+                }
+                {/* <ItemCount2 stock={item.stock}  onAdd={onAdd}  /> */}
             </div>
         </div>
         // </Link>
--- src/componentes/tarjetas.js
+++ src/componentes/tarjetas.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ItemCount2 from "./itemcount2";
+// import ItemCount2 from "./itemcount2";
 const tarjeta = ({info}) =>{
 
     
-      const onAdd = (e) => {
-          console.log(` Productos seleccionados ${e}`)
-      }
+    //   const onAdd = (e) => {
+    //       console.log(` Productos seleccionados ${e}`)
+    //   }
     return(
     <div className="navegacion">
   <div class="col-12 col-md-6">
@@ -16,8 +16,8 @@ const tarjeta = ({info}) =>{
               <h3 class="h5 card-title mt-3">{info.titulo}</h3>
               <p class="card-text">{info.descripcion}</p>
               <p class="card-text">$ {info.precio}</p>
-              <Link to={`/detalle/${info.id}`} className="btn "> Ver mas</Link>
-                            <ItemCount2 stock={info.stock}  onAdd={onAdd}  />
+              <Link to={`/detalle/${info.id}`} className="btn btn-primary"> Ver mas</Link>
+                            {/* <ItemCount2 stock={info.stock}  onAdd={onAdd}  /> */}
           </div>
       </div>
   </div>
